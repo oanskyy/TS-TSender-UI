9af420c52eb0474d512b992567e737bc9957843d
oanskyy <oana.anghelachi@gmail.com>
2025-06-27 04:26:43 +0100

    chore(config): convert Next.js project to static export for Netlify deployment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,12 @@
 import AirdropForm from '../components/AirdropForm';
 
 export default function Home() {
+  // eslint-disable-next-line no-console
+  console.log(
+    'WalletConnect ID:',
+    process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID,
+  );
+
   return (
     <div className="grid min-h-screen p-8 pb-20 gap-12 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       {/* Main content */}
