0df4e6118ad6d0ea59c8bf7f316b2882a3f3268b
oanskyy <oana.anghelachi@gmail.com>
2025-07-21 02:50:33 +0100

    test: check husky

--- src/rainbowKitConfig.tsx
+++ src/rainbowKitConfig.tsx
@@ -14,6 +14,7 @@ import {
 export default getDefaultConfig({
   appName: 'TSender UI',
   projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID!,
+
   chains: [anvil, zksync, mainnet, optimism, arbitrum, base, sepolia],
   ssr: false, // If your dApp uses server side rendering (SSR)
 });
